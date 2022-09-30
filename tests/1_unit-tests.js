3ec31f6591567b3e1234b60c44322a0fbc943993
Raheem Mubarak <raheemmubarak7@gmail.com>
2022-09-30 23:37:25 +0100

    c

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,7 +5,10 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.isNull(null, 'This is an optional error description - e.g. null is null');
+      assert.isNull(
+        null,
+        'this is an optional error description - e.g. null is null'
+      );
       assert.isNotNull(1, '1 is not null');
     });
     // #2
